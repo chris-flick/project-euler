283760bb45a0711d5a646451ca8787cdca4c20d4
Flickinger, Christian <flickingerce@upmc.edu>
2020-10-20 14:11:25 -0400

    removing extra folder

--- project-euler/multiples_of_3_and_5/multiples_of_3_and_5.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace multiples_of_3_and_5 {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
