e2e41c87a73d6bf560936832a5c262a37c133df4
Christian Flickinger <cef39@pitt.edu>
2020-10-20 14:02:16 -0400

    starting c# project

---
+++ project-euler/multiples_of_3_and_5/multiples_of_3_and_5.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace multiples_of_3_and_5 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
