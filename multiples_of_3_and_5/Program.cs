7297f53ccd52d2e42af4bdcab7eaeb705b2a056c
Flickinger, Christian <flickingerce@upmc.edu>
2020-10-21 12:52:49 -0400

    added .gitignore to project

--- multiples_of_3_and_5/Program.cs
+++ multiples_of_3_and_5/Program.cs
@@ -8,6 +8,14 @@ namespace multiples_of_3_and_5
     {
     class Program {
 
+        /// <summary>
+        /// 
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// 
+        /// </summary>
+        /// <param name="args">The number to find the sum of all the multiples of 3 or 5 for.</param>
+        /// <returns></returns>
         static int Main(string[] args) {
 
             // validate that user provided a value
